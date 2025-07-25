60f028c136ab41c486a15b3988f7c75f6f4e8083
Elder <emaildoelder@gmail.com>
2025-07-25 18:04:33 -0300

    Else e comentários

--- JavaScript/JogoSecreto/app.js
+++ JavaScript/JogoSecreto/app.js
@@ -3,5 +3,7 @@ let numeroSecreto = 29;
 let chute = prompt('Escolha um número entre 1 e 30');
 
 if (chute == numeroSecreto){
-    console.log('Isso ai! Você descobriu o número secreto');
+    alert('Isso ai! Você descobriu o número secreto');
+}else{
+    alert('Você errou :(')
 }
