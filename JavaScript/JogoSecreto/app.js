31986955516b969a3d42ba5fbf8f61ad004b0d09
Elder <emaildoelder@gmail.com>
2025-07-25 17:45:48 -0300

    Comparando variáveis

--- JavaScript/JogoSecreto/app.js
+++ JavaScript/JogoSecreto/app.js
@@ -1,5 +1,7 @@
 alert('Bem-vindo ao Jogo do Número Secreto');
-
 let numeroSecreto = 29;
+let chute = prompt('Escolha um número entre 1 e 30');
 
-prompt('Escolha um número entre 1 e 30');
+if (chute == numeroSecreto){
+    console.log('Isso ai! Você descobriu o número secreto');
+}
