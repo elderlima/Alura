58cb59bcbd113bca4f7bb15a1e975c966e0e9287
Elder <emaildoelder@gmail.com>
2025-07-27 07:43:28 -0300

    Novas condições

--- JavaScript/JogoSecreto/app.js
+++ JavaScript/JogoSecreto/app.js
@@ -5,6 +5,10 @@ let chute = prompt('Escolha um número entre 1 e 30');
 
 if (chute == numeroSecreto){
     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
-}else{
-    alert('Você errou :(')
+    }else{
+        if (chute > numeroSecreto){
+            alert(`O número secreto é menor que ${chute}`);
+            }else{
+                alert(`O número secreto é maior que ${chute}`);
+                }
 }
