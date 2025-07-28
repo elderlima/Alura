300432aa57ade1d8517d959f55baa9e59cf19429
Elder <emaildoelder@gmail.com>
2025-07-28 07:15:36 -0300

    Adicionando contador de tentativas

--- JavaScript/JogoSecreto/app.js
+++ JavaScript/JogoSecreto/app.js
@@ -1,19 +1,21 @@
 alert('Bem-vindo ao Jogo do Número Secreto');
 let numeroSecreto = 7;
 console.log(numeroSecreto);
-let chute ;
+let chute;
+let tentativas=1;
 
 while(chute != numeroSecreto){
     chute = prompt('Escolha um número entre 1 e 12');
 
     if (chute == numeroSecreto){
-        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas}`);
     }else{
         if (chute > numeroSecreto){
             alert(`O número secreto é menor que ${chute}`);
         }else{
                 alert(`O número secreto é maior que ${chute}`);
         }
+        tentativas++;
     }
 
 }
