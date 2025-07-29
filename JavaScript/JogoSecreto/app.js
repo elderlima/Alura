f17dcd90098dcc469b42e0378e49b9044857f216
Elder <emaildoelder@gmail.com>
2025-07-29 07:28:19 -0300

    Contando tentativas

--- JavaScript/JogoSecreto/app.js
+++ JavaScript/JogoSecreto/app.js
@@ -38,6 +38,7 @@
 // paragrafo.innerHTML = 'Escolha o número entre 1 e 10';
 
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
@@ -49,15 +50,21 @@ exibirTextoNaTela('p', 'Escolha o número entre 1 e 10');
 
 function verificarChute(){
     let chute = document.querySelector('input').value;
+
     if(chute == numeroSecreto){
         exibirTextoNaTela('h1', 'Acertou!');
-        exibirTextoNaTela('p','Você descobriu o número secreto!');
+
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}` ;
+
+        exibirTextoNaTela('p',mensagemTentativas);
     }else{
         if (chute > numeroSecreto){
-            exibirTextoNaTela(`p`, `O número secreto é menor!.`);
+            exibirTextoNaTela(`p`, `O número secreto é menor!`);
         }else{
-            exibirTextoNaTela(`p`, `O número secreto é maior!.`);
+            exibirTextoNaTela(`p`, `O número secreto é maior!`);
         }
+        tentativas++;
     }
 }
 
