258487a90c03ad2efdae54fd444af1ff7366e13a
Elder <emaildoelder@gmail.com>
2025-08-01 09:41:41 -0300

    Botão reiniciar

--- JavaScript/JogoSecreto/app.js
+++ JavaScript/JogoSecreto/app.js
@@ -1,42 +1,3 @@
-// ##### Aula 1
-//
-// let numeroSecreto = gerarNumeroAleatorio();
-// let tentativas = 1;
-
-// function exibirTextoNaTela(tag, texto){
-//     let campo = document.querySelector(tag);
-//     campo.innerHTML = texto;
-// }
-
-// exibirTextoNaTela(`h1`, `Jogo do Número Secreto!`);
-// exibirTextoNaTela(`p`, `Escolha um número entre 1 a 12.`);
-
-// function verificarChute(){
-//     let chute = document.querySelector(`input`).value;
-//     if (chute == numeroSecreto){
-//         exibirTextoNaTela(`h1`, `Acertou!`);
-//         exibirTextoNaTela(`p`, `Você Descobriu o número secreto com x tentativas.`);
-//     }else{
-//         if (chute > numeroSecreto){
-//             exibirTextoNaTela(`p`, `O número secreto é menor!.`);
-//         }else{
-//             exibirTextoNaTela(`p`, `O número secreto é maior!.`);
-//         }
-//     }
-// }
-
-// function gerarNumeroAleatorio() {
-//     return parseInt(Math.random() * 12 + 1);
-// }
-
-//##### Aula 2
-
-// let titulo = document.querySelector('h1');
-// titulo.innerHTML = 'Jogo do Número Secreto';
-
-// let paragrafo = document.querySelector('p');
-// paragrafo.innerHTML = 'Escolha o número entre 1 e 10';
-
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -58,6 +19,8 @@ function verificarChute(){
         let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}` ;
 
         exibirTextoNaTela('p',mensagemTentativas);
+
+        document.getElementById('reiniciar').removeAttribute('disabled');
     }else{
         if (chute > numeroSecreto){
             exibirTextoNaTela(`p`, `O número secreto é menor!`);
@@ -65,9 +28,15 @@ function verificarChute(){
             exibirTextoNaTela(`p`, `O número secreto é maior!`);
         }
         tentativas++;
+        limparCampo();
     }
 }
 
 function gerarNumeroAleatorio(){
     return parseInt(Math.random() * 10 +1);
+}
+
+function limparCampo(){
+    chute = document.querySelector('input');
+    chute.value = '';
 }
